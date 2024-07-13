da0c78d9407b79f23f8a88f87b32278105791a46
Joseph <iossif123gg@gmail.com>
2024-07-13 09:26:09 +0300

    boilerplate part 1

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import BasicTable from './BasicTable';
+import ButtonUsage from './Test';
+
+function App() {
+	return (
+		<>
+			<ButtonUsage />
+			<BasicTable />
+		</>
+	);
+}
+
+export default App;
---
+++ client/src/main.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createTheme, ThemeProvider } from '@mui/material';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const darkTheme = createTheme({
+	palette: {
+		mode: 'dark',
+	},
+});
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+	<React.StrictMode>
+		<ThemeProvider theme={darkTheme}>
+			<CssBaseline />
+			<App />
+		</ThemeProvider>
+	</React.StrictMode>
+);
